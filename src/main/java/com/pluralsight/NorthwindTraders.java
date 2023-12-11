2010f310d03d1fc7955381218824506bfa16361f
paulmlaviwa <mlaviwa.paul@gmail.com>
2023-12-11 15:47:34 -0500

    Added executeQuery statement to display multiple columns

--- src/main/java/com/pluralsight/NorthwindTraders.java
+++ src/main/java/com/pluralsight/NorthwindTraders.java
@@ -21,13 +21,20 @@ public class NorthwindTraders {
             Statement statement = connection.createStatement();
 
             // Execute a query to select all products
-            String query = "SELECT ProductName FROM Products";
+            String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products";
             ResultSet resultSet = statement.executeQuery(query);
 
-            // Display the name of each product
+            // Display the info of each product
             while (resultSet.next()) {
+                int productID = resultSet.getInt("ProductID");
                 String productName = resultSet.getString("ProductName");
-                System.out.println(productName);
+                double unitPrice = resultSet.getDouble("UnitPrice");
+                int unitsInStock = resultSet.getInt("UnitsInStock");
+
+                System.out.println("ProductID: " + productID +
+                        ", ProductName: " + productName +
+                        ", UnitPrice: " + unitPrice +
+                        ", UnitsInStock: " + unitsInStock);
             }
 
             // Close resources
