8b7e9a547f17eea2a4b198f9bea70f5e07ed18da
paulmlaviwa <mlaviwa.paul@gmail.com>
2023-12-12 11:40:19 -0500

    Widened the columns

--- src/main/java/com/pluralsight/NorthwindTraders.java
+++ src/main/java/com/pluralsight/NorthwindTraders.java
@@ -50,8 +50,8 @@ public class NorthwindTraders {
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products")
         ) {
-            System.out.printf("%-10s %-30s %-15s %-10s%n", "ID", "Name", "Price", "Stock");
-            System.out.println("------------------------------------------------------------------");
+            System.out.printf("%-15s %-40s %-15s %-10s%n", "Id", "Name", "Price", "Stock");
+            System.out.println("------------------------------------------------------------------------------------");
 
             while (resultSet.next()) {
                 int productID = resultSet.getInt("ProductID");
@@ -59,7 +59,7 @@ public class NorthwindTraders {
                 double unitPrice = resultSet.getDouble("UnitPrice");
                 int unitsInStock = resultSet.getInt("UnitsInStock");
 
-                System.out.printf("%-10d %-30s %-15.2f %-10d%n", productID, productName, unitPrice, unitsInStock);
+                System.out.printf("%-15d %-40s %-15.2f %-10d%n", productID, productName, unitPrice, unitsInStock);
             }
             System.out.println();
         }
@@ -71,8 +71,8 @@ public class NorthwindTraders {
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery("SELECT ContactName, CompanyName, City, Country, Phone FROM Customers ORDER BY Country")
         ) {
-            System.out.printf("%-20s %-30s %-20s %-15s %-15s%n", "ContactName", "CompanyName", "City", "Country", "Phone");
-            System.out.println("------------------------------------------------------------------------------------------------");
+            System.out.printf("%-25s %-40s %-20s %-15s %-15s%n", "ContactName", "CompanyName", "City", "Country", "Phone");
+            System.out.println("--------------------------------------------------------------------------------------------------------------------");
 
             while (resultSet.next()) {
                 String contactName = resultSet.getString("ContactName");
@@ -81,8 +81,9 @@ public class NorthwindTraders {
                 String country = resultSet.getString("Country");
                 String phone = resultSet.getString("Phone");
 
-                System.out.printf("%-20s %-30s %-20s %-15s %-15s%n", contactName, companyName, city, country, phone);
+                System.out.printf("%-25s %-40s %-20s %-15s %-15s%n", contactName, companyName, city, country, phone);
             }
+            System.out.println();
         }
     }
 }
