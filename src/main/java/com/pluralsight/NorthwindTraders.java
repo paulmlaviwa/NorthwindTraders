d09b221797f6855c7bf767b5fb5097a0d545b0d8
paulmlaviwa <mlaviwa.paul@gmail.com>
2023-12-12 11:27:51 -0500

    Created Home Screen for user to choose what they want

--- src/main/java/com/pluralsight/NorthwindTraders.java
+++ src/main/java/com/pluralsight/NorthwindTraders.java
@@ -5,44 +5,84 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class NorthwindTraders {
     public static void main(String[] args) {
         // Opening a connection to the DB
         String url = "jdbc:mysql://localhost:3306/northwind";
         String user = "root";
-        String password = "*******";
+        String password = "@Cyantist.93_";
 
-        try {
-            // Establish the connection
-            Connection connection = DriverManager.getConnection(url, user, password);
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                // Display home screen
+                System.out.println("What do you want to do?");
+                System.out.println("1) Display all products");
+                System.out.println("2) Display all customers");
+                System.out.println("0) Exit");
+                System.out.print("Select an option: ");
 
-            // Create a statement
-            Statement statement = connection.createStatement();
+                int choice = scanner.nextInt();
 
-            // Execute a query to select all products
-            String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products";
-            ResultSet resultSet = statement.executeQuery(query);
+                switch (choice) {
+                    case 1:
+                        displayAllProducts(url, user, password);
+                        break;
+                    case 2:
+                        displayAllCustomers(url, user, password);
+                        break;
+                    case 0:
+                        System.out.println("Exiting program. Goodbye!");
+                        return;
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-            // Display the info of each product
+    private static void displayAllProducts(String url, String user, String password) throws SQLException {
+        try (
+                Connection connection = DriverManager.getConnection(url, user, password);
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products")
+        ) {
+            System.out.printf("%-10s %-30s %-15s %-10s%n", "ID", "Name", "Price", "Stock");
+            System.out.println("------------------------------------------------------------------");
+
             while (resultSet.next()) {
                 int productID = resultSet.getInt("ProductID");
                 String productName = resultSet.getString("ProductName");
                 double unitPrice = resultSet.getDouble("UnitPrice");
                 int unitsInStock = resultSet.getInt("UnitsInStock");
 
-                System.out.println("ProductID: " + productID +
-                        ", ProductName: " + productName +
-                        ", UnitPrice: " + unitPrice +
-                        ", UnitsInStock: " + unitsInStock);
+                System.out.printf("%-10d %-30s %-15.2f %-10d%n", productID, productName, unitPrice, unitsInStock);
             }
+            System.out.println();
+        }
+    }
 
-            // Close resources
-            resultSet.close();
-            statement.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+    private static void displayAllCustomers(String url, String user, String password) throws SQLException {
+        try (
+                Connection connection = DriverManager.getConnection(url, user, password);
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT ContactName, CompanyName, City, Country, Phone FROM Customers ORDER BY Country")
+        ) {
+            System.out.printf("%-20s %-30s %-20s %-15s %-15s%n", "ContactName", "CompanyName", "City", "Country", "Phone");
+            System.out.println("------------------------------------------------------------------------------------------------");
+
+            while (resultSet.next()) {
+                String contactName = resultSet.getString("ContactName");
+                String companyName = resultSet.getString("CompanyName");
+                String city = resultSet.getString("City");
+                String country = resultSet.getString("Country");
+                String phone = resultSet.getString("Phone");
+
+                System.out.printf("%-20s %-30s %-20s %-15s %-15s%n", contactName, companyName, city, country, phone);
+            }
         }
     }
 }
